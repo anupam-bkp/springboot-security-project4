99bbc4fd2abfbb730eb287f76957d018e5ca51b7
anupam <anupam.nitdgp14@gmail.com>
2025-02-19 18:23:08 +0530

    setting up the springboot project configuration.

---
+++ src/main/java/com/learner/eazybank/SpringbootSecurityProject4Application.java
@@ -0,0 +1,13 @@
+package com.learner.eazybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityProject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityProject4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learner/eazybank/SpringbootSecurityProject4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learner.eazybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityProject4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
